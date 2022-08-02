94ac8ab7bfa2e99c6445df87d7409f6bd6f91a97
Marco <marco@esplores.com>
2022-08-02 12:19:25 +0200

    Map local folders for annotations and images inside the container and run the populate_dirlist.sh script to generate the updated image list

--- DockerFiles/ubuntu_16.04/start_labelme_container.sh
+++ DockerFiles/ubuntu_16.04/start_labelme_container.sh
@@ -4,6 +4,8 @@ docker run \
 --name labelme \
 -p 8080:80 \
 -d \
+-v /usr/local/labelmeannotationtool/images:/var/www/html/LabelMeAnnotationTool/Images \
+-v /usr/local/labelmeannotationtool/annotations:/var/www/html/LabelMeAnnotationTool/Annotations \
 --entrypoint "/bin/bash" \
 -t labelme
 
